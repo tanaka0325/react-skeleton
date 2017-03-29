9d8380b11f27bd179a3328c958f399c10f5506c8
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-03-30 01:38:23 +0900

    Update build command

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,10 +1,12 @@
 import path from 'path'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 module.exports = [
   {
-    entry: path.join(__dirname, 'src', 'index.js'),
+    name: 'js',
+    entry: path.join(__dirname, 'src', 'js', 'index.js'),
     output: {
-      path: path.join('dest'),
+      path: path.join(__dirname, 'build'),
       filename: 'bundle.js',
     },
     target: 'web',
@@ -18,7 +20,77 @@ module.exports = [
       ],
     },
     resolve: {
-      extensions: ['', '.js', '.jsx'],
+      extensions: ['.js', '.jsx'],
     },
   },
+  {
+    name: 'html',
+    entry: path.join(__dirname, 'src', 'index.html'),
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'index.html',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          use: ExtractTextPlugin.extract('raw-loader')
+        },
+      ],
+    },
+    plugins: [
+      new ExtractTextPlugin('index.html'),
+    ],
+  },
+  {
+    name: 'style',
+    entry: path.join(__dirname, 'src', 'sass', 'style.sass'),
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'style.css',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.sass$/,
+          exclude: /node_modules/,
+          use: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
+        },
+        {
+          test: /\.(jpg|png)$/,
+          exclude: /node_modules/,
+          loader: 'url-loader',
+        },
+      ],
+    },
+    plugins: [
+      new ExtractTextPlugin('style.css'),
+    ],
+  },
 ]
+
+// import path from 'path'
+// 
+// module.exports = [
+//   {
+//     entry: path.join(__dirname, 'src', 'index.js'),
+//     output: {
+//       path: path.join('dest'),
+//       filename: 'bundle.js',
+//     },
+//     target: 'web',
+//     module: {
+//       loaders: [
+//         {
+//           test: /\.js$/,
+//           exclude: /node_modules/,
+//           loader: 'babel-loader',
+//         },
+//       ],
+//     },
+//     resolve: {
+//       extensions: ['', '.js', '.jsx'],
+//     },
+//   },
+// ]
