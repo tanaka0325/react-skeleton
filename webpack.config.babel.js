59c136ee18541c37e45568da0c2f614c21347c83
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-03-30 15:28:32 +0900

    Remove unused code

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -69,28 +69,3 @@ module.exports = [
     ],
   },
 ]
-
-// import path from 'path'
-// 
-// module.exports = [
-//   {
-//     entry: path.join(__dirname, 'src', 'index.js'),
-//     output: {
-//       path: path.join('dest'),
-//       filename: 'bundle.js',
-//     },
-//     target: 'web',
-//     module: {
-//       loaders: [
-//         {
-//           test: /\.js$/,
-//           exclude: /node_modules/,
-//           loader: 'babel-loader',
-//         },
-//       ],
-//     },
-//     resolve: {
-//       extensions: ['', '.js', '.jsx'],
-//     },
-//   },
-// ]
